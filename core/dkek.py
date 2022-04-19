89559f494aeaafd0491725391b9896ed1d837151
david barkhuizen <david.barkhuizen@gmail.com>
2022-04-19 06:59:50 +0200

    unpacking first element of asn1.der going well

--- core/dkek.py
+++ core/dkek.py
@@ -161,23 +161,37 @@ def encrypt_dkek_share_blob(share: bytes, password: bytes, salt: bytes = None):
 # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 def decrypt_wrapped_key_blob(dkek: bytes, blob: bytes):
-    
-    xxx = unhexlify('97e303855dc1c54d0c000a04007f00070202020203000000000000b2c97abaf6bf457fd94d5d76eb668dd10d042b3aebacc4f5b36f723e1ca129c0b6676f2b63ba6accf71923b6bd8d2c3a9190e19722c763c3082c35605caaa4d2c2eef72b42c5b5040da7f9a7a5450d33f49722137f479f9f34bf8d79d08ff109c756edb3c1ff77b5b2edc548da72629708bc618f5c262591420d45c5decb6cc5cd4aef328c8320aa90aa1f755f0aaee27af363cc5db5d21d912570866884c491c51325df73194bb641fd74080e49559a3a11a96c29eccddbf8dcad16352588a8b58990adf360df03e0e9d6081772d89a33b38931c64ebf4f9c8a3aa10fc87d71525a299401739531c0856b694bc124b2df87183bec684824bf57dba60ca511f3be96fcf16c06a4c4ad9c5c844d04d0f3d8a95208ea818d92263d1b754b0a840bd41b8d12f328b891f7bbad8f84806f5cfc24faa7b38b369fb98efdb77bccfb5af27e6021516b79619479e6138663277d')
 
-    dkek_kcv = xxx[:8]
+    # TODO parse asn.1
+    # blob below is actually the first element (an octet string) in an asn.1 SEQUENCE of l=3
+    #
+    # SEQUENCE (3 elem)
+    #   OCTET STRING (363 byte) 97E303855DC1C54D0C000A04007F00070202020203000000000000B2C97ABAF6BF457…
+    #   ... (2)
+    #   ... (3)
+
+    blob = unhexlify('97e303855dc1c54d0c000a04007f00070202020203000000000000b2c97abaf6bf457fd94d5d76eb668dd10d042b3aebacc4f5b36f723e1ca129c0b6676f2b63ba6accf71923b6bd8d2c3a9190e19722c763c3082c35605caaa4d2c2eef72b42c5b5040da7f9a7a5450d33f49722137f479f9f34bf8d79d08ff109c756edb3c1ff77b5b2edc548da72629708bc618f5c262591420d45c5decb6cc5cd4aef328c8320aa90aa1f755f0aaee27af363cc5db5d21d912570866884c491c51325df73194bb641fd74080e49559a3a11a96c29eccddbf8dcad16352588a8b58990adf360df03e0e9d6081772d89a33b38931c64ebf4f9c8a3aa10fc87d71525a299401739531c0856b694bc124b2df87183bec684824bf57dba60ca511f3be96fcf16c06a4c4ad9c5c844d04d0f3d8a95208ea818d92263d1b754b0a840bd41b8d12f328b891f7bbad8f84806f5cfc24faa7b38b369fb98efdb77bccfb5af27e6021516b79619479e6138663277d')
+
+    def generate_unpacker(source: bytes):
+
+        def get_obj_from_blob(offset: int):
+            [l] = struct.unpack('>H', source[offset:offset+2])
+            obj = source[offset+2:offset+2+l]
+            return (obj, l, offset+2+l)
+
+        return get_obj_from_blob
+
+    dkek_kcv = blob[:8]
     print(f'DKEK KCV {hexlify(dkek_kcv)}')
 
-    raw_key_type = xxx[8]
-    print(f'raw key type {raw_key_type}')
+    raw_key_type = blob[8]   
     key_type = KeyType(raw_key_type)
-    print(f'key type {key_type.name}')
+    print(f'key type {key_type.name} ({raw_key_type})')
+
+    get_blob_obj_at = generate_unpacker(blob)
 
     offset = 9
-    l = 2
-
-    [l] = struct.unpack('>H', xxx[offset:offset+l])
-    print(l)
-    oid = xxx[offset+2:offset+2+l]
+    [oid, obj_len,offset] = get_blob_obj_at(offset)
     print(hexlify(oid))
     # id-TA-ECDSA-SHA-256 0.4.0.127.0.7.2.2.2.2.3
 
@@ -185,41 +199,53 @@ def decrypt_wrapped_key_blob(dkek: bytes, blob: bytes):
     # 0000 access conditions
     # 0000 key OID
 
-    offset = offset + 2 + l
-    l = 2
-    [l] = struct.unpack('>H', xxx[offset:offset+l])
-    print(l)
-    allowed_algos = xxx[offset+2:offset+2+l]
+    [allowed_algos, size, offset] = get_blob_obj_at(offset)
     print(f'allowed_algos: {hexlify(allowed_algos)}')
 
-    offset = offset + 2 + l
-    l = 2
-    [l] = struct.unpack('>H', xxx[offset:offset+l])
-    print(l)
-    access_conditions = xxx[offset+2:offset+2+l]
+    [access_conditions, size, offset] = get_blob_obj_at(offset)
     print(f'access_conditions: {hexlify(access_conditions)}')
 
-    offset = offset + 2 + l
-    l = 2
-    [l] = struct.unpack('>H', xxx[offset:offset+l])
-    print(l)
-    key_oid = xxx[offset+2:offset+2+l]
+    [key_oid, size, offset] = get_blob_obj_at(offset)
     print(f'key_oid: {hexlify(key_oid)}')
 
-    offset = offset + 2 + l
-    encrypted_blob = xxx[offset:-16]
+    encrypted_blob = blob[offset:-16] # TODO check
     print(len(encrypted_blob), hexlify(encrypted_blob))
 
-    decrypted_blob = decrypt_aes_cbc(derive_kek_from_dkek(dkek), bytes([0x00]*16), encrypted_blob)
+    kek = derive_kek_from_dkek(dkek)
+    iv = bytes([0x00]*16)
+    decrypted_blob = decrypt_aes_cbc(kek, iv, encrypted_blob)
     print(f'decrypted_blob: {hexlify(decrypted_blob)}')
 
     random_prefix = decrypted_blob[:8]
     print(f'random_prefix: {hexlify(random_prefix)}')
 
+    get_key_obj_at = generate_unpacker(decrypted_blob)
+
     [key_size] = struct.unpack('>H', decrypted_blob[8:10])
     print(f'key size, bits: {key_size}')
 
+    offset = 10
+    
+    [a, size, offset] = get_key_obj_at(offset)
+    print(f'a: {hexlify(a)}')
 
+    [b, size, offset] = get_key_obj_at(offset)
+    print(f'b: {hexlify(b)}')
+
+    [prime_factor, size, offset] = get_key_obj_at(offset)
+    print(f'prime_factor: {hexlify(prime_factor)}')
+
+    [order, size, offset] = get_key_obj_at(offset)
+    print(f'order: {hexlify(order)}')
+
+    [generator_g, size, offset] = get_key_obj_at(offset)
+    print(f'generator_g: {hexlify(generator_g)}')
+
+    [secret_d, size, offset] = get_key_obj_at(offset)
+    print(f'secret_d: {hexlify(secret_d)}')
+
+    [pub_point_q, size, offset] = get_key_obj_at(offset)
+    print(f'pub_point_q: {hexlify(pub_point_q)}')
 
     # mak = derive_mak_from_dkek(dkek)
     # print(f'len(mak) {len(mak)}')
@@ -227,6 +253,3 @@ def decrypt_wrapped_key_blob(dkek: bytes, blob: bytes):
 
     # print(f'blob cmac   {hex(blob_cmac)}')
     # print(f'calced cmac {hex(calced_cmac)}')
-
-
-
