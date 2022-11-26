e4bf284fb18d9a1d481bc5dc9b65d066375e5ebd
davidbarkhuizen <david.barkhuizen@gmail.com>
2022-11-26 10:19:32 +0200

    core tests run in py3

---
+++ install.sh
@@ -0,0 +1,4 @@
+python -m venv .py3
+source .py3/bin/activate
+python -m pip install -r requirements.txt
+deactivate
--- test.sh
+++ test.sh
@@ -1,4 +1,8 @@
+source .py3/bin/activate
+
 python -m unittest -v tests/dkek-tests.py
 python -m unittest -v tests/wrapped-key-tests.py
 python -m unittest -v tests/ec-tests.py
 python -m unittest -v tests/ecdsa-tests.py
+
+deactivate
